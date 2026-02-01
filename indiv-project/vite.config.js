d50b0eaa4efe734afc022e6447ab3557734fc440
kimitra <vmitraki@purdue.edu>
2026-01-31 19:45:22 -0600

    Deploy

--- indiv-project/vite.config.js
+++ indiv-project/vite.config.js
@@ -3,11 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react({
-      babel: {
-        plugins: [['babel-plugin-react-compiler']],
-      },
-    }),
-  ],
+  plugins: [react()],
+  base: '/Individual-Project/'
 })
+    
